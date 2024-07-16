21110f7a3c7cafe1719039567742843f027810d0
Aakash Shrestha <168775432+axtha-np@users.noreply.github.com>
2024-07-15 23:36:51 -0700

    Add files via upload
    For parallel run

---
+++ parallel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+prot=$1
+target=$2
+
+for (( i=1 ; i<=$3 ; i++ ));
+do
+sbatch submitp.sh ${prot} $target $i
+done
---
+++ submitp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1            # number of nodes
+#SBATCH -c 4            # number of cores 
+#SBATCH -p general      # partition
+#SBATCH -t 0-06:00:00   # time in d-hh:mm:ss 
+#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --export=NONE   # Purge the job-submitting shell environment
+
+prot = $1
+
+/packages/envs/pyrosetta-2023/bin/python designparallel.py -t $2 -f {prot}.pdb -r $3
